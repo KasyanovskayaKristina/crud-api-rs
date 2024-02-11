75286829b6f84f884c43b9296d43a5746dc43bd6
Kasyanovskaya Kristina <kristinochkakv@gmail.com>
2024-02-11 17:10:41 +0300

    feat: add webpack and change basic settings

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageProvider: 'v8',
+  moduleFileExtensions: ['js', 'ts'],
+  rootDir: './test',
+  testMatch: ['**/test/user-test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  }
+};
+
+export default config;
